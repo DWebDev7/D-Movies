b1346a471f1800d868e52c5ef2bfb7aefc528dfe
DeePrime7 <otenedaniel007@gmail.com>
2025-01-20 00:45:19 +0100

    Auto-commit

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const MovieCard = ({ movie }) => {
   return (
-    <div className="movie">
-        <div>
-            <p>{movie.Year}</p>
-        </div>
-        <div>
-            <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt="movieposter" />
-        </div>
-        <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-        </div>
+    <div className="relative h-80 w-56 rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105">
+      <div className="absolute top-0 left-0 p-4 text-yellow-300 opacity-0 hover:opacity-100 transition-opacity">
+        <p>{movie.release_date}</p>
+      </div>
+      <div className="h-80 w-56">
+        <img
+          src={
+            movie.poster !== "N/A"
+              ? movie.poster
+              : "https://via.placeholder.com/400"
+          }
+          alt="movieposter"
+          className="h-full w-full rounded-md"
+        />
+      </div>
+      <div className="mt-4">
+        <h3 className="text-lg font-bold text-yellow-400">{movie.title}</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
